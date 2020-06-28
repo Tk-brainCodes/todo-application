25ed54de1b7e3df9239ad82b202dbb7a03c746d1
Tk-brainCodes <54640421+Tk-brainCodes@users.noreply.github.com>
2020-06-28 20:57:55 +0100

    initial commit

---
+++ build/precache-manifest.1db73ad47535ca97d738ea647497db82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5354f45debf2754f2a396f3ffac2d2d",
+    "url": "/todo-application/index.html"
+  },
+  {
+    "revision": "feaf1305abb45f9d7cff",
+    "url": "/todo-application/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "603b098259e5e96b74e5",
+    "url": "/todo-application/static/js/2.09a580dc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-application/static/js/2.09a580dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feaf1305abb45f9d7cff",
+    "url": "/todo-application/static/js/main.d77cc0e7.chunk.js"
+  },
+  {
+    "revision": "e29d0b0f3287792638e3",
+    "url": "/todo-application/static/js/runtime-main.9c58e7a6.js"
+  }
+]);
