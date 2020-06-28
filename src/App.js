00b45030dc746afd9e6809568966762cedb524e7
Tk-brainCodes <54640421+Tk-brainCodes@users.noreply.github.com>
2020-06-28 20:38:54 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, {Component} from 'react';
 import TodoApp from './todo-app/todo';
-import SWMovies from './Starwar/starwar';
-import Section from './Demo/Section';
-import Navbar from './Demo/Navbar';
-import About from './Demo/About';
-import Skills from './Demo/Skills';
-import './Demo/navbar.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       {/*
-               <Navbar />
-        <Section
-          id="section1"
-        />
-        <About
-          id="About"
-        />
-        <Skills id ="Skills"/>
-
-       */}
        <TodoApp/>
       </div>
     );
